c35b8edf807f75885409915c5fc0441f0310decf
raphaeltomagnini <raphaeltomagnini@gmail.com>
2023-03-14 21:16:18 -0300

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,14 @@ const App = () => {
        
     useEffect(() => {
         const fetchTasks = async () => {
-            const { data } = await axios.get(
-              "https://jsonplaceholder.cypress.io/todos?_limit=10"
-            );
-              setTasks(data);
+            try {
+              const { data } = await axios.get(
+                "https://jsonplaceholder.cypress.io/todos?_limit=10"
+              );
+                setTasks(data);
+            } catch (error) {
+              console.log (error)
+            }
         };
         fetchTasks();
         }, []);
